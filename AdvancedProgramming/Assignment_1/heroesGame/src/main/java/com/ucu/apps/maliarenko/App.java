18cf946c8bb3c9ed64c0a995f66f0ef055a0f59e
maliarenko <maliarenko@ucu.edu.ua>
2017-10-24 20:51:27 +0300

    task_3

--- AdvancedProgramming/Assignment_1/heroesGame/src/main/java/com/ucu/apps/maliarenko/App.java
+++ AdvancedProgramming/Assignment_1/heroesGame/src/main/java/com/ucu/apps/maliarenko/App.java
@@ -1,9 +1,9 @@
 package com.ucu.apps.maliarenko;
 
+import com.github.javafaker.Faker;
 import com.ucu.apps.maliarenko.factory.CharacterFactory;
 import com.ucu.apps.maliarenko.manager.GameManager;
 import com.ucu.apps.maliarenko.model.Character;
-import com.ucu.apps.maliarenko.model.Fighter;
 
 /**
  * Heroes Game!
@@ -14,11 +14,15 @@ public class App
     private static CharacterFactory characterFactory;
     public static void main( String[] args ) {
 
+        Faker faker = new Faker();
+
         Character c1 = characterFactory.createCharacter();
-        System.out.println(c1.toString());
+        c1.setName(faker.name().firstName());
 
         Character c2 = characterFactory.createCharacter();
-        System.out.println(c2.toString());
+        c2.setName(faker.name().firstName());
+
+        System.out.println(c1.toString() + " vs " + c2.toString());
 
         GameManager gameManager = new GameManager();
         gameManager.fight(c1, c2);
