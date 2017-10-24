92a39da30e7daaf3c46cd377fb0284cc61e262f9
maliarenko <maliarenko@ucu.edu.ua>
2017-10-24 17:51:08 +0300

    task_1

--- AdvancedProgramming/Assignment_1/Task_1/src/main/java/com/maliarenko/a1/App.java
+++ AdvancedProgramming/Assignment_1/Task_1/src/main/java/com/maliarenko/a1/App.java
@@ -1,13 +1,39 @@
 package com.maliarenko.a1;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.IntBinaryOperator;
+
 /**
- * Hello world!
+ * Switch case replacement!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        //one variant
+        HashMap<Integer, String> weekdays = new HashMap<>();
+
+        weekdays.put(0,"Mo");
+        weekdays.put(1,"Tu");
+        weekdays.put(2,"We");
+        weekdays.put(3,"Th");
+        weekdays.put(4,"Fr");
+        weekdays.put(5,"Sa");
+        weekdays.put(6,"Su");
+
+        int dayNumber = 4;
+        System.out.println(weekdays.get(dayNumber));
+
+
+        //another case with lambda
+        Map<String, IntBinaryOperator> operations = new HashMap<>();
+        operations.put("subtract", (a, b) -> a - b);
+        operations.put("multiply", (a, b) -> a * b);
+
+        System.out.println(operations.get("multiply").applyAsInt(10, 20));
+
+        //another one I used in task_3 (HeroesGame) - in CharacterFactory
     }
 }
